df8f7adc6e21fbf1d769a2dc503fdb0168ee9ead
gknoechel <grant.knoechel@gmail.com>
2016-02-22 10:47:17 -0500

    Buffered reader

--- Main/src/FileManager.java
+++ Main/src/FileManager.java
@@ -1,6 +1,8 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by mccloskeybr on 2/16/16.
@@ -31,4 +33,19 @@ public class FileManager {
 
         return null;
     }
+
+    public void readLines(String path) throws IOException
+    {
+        File file = new File(path);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        ArrayList<String> actors = new ArrayList<String>();
+
+        String line = br.readLine();
+        while(line!=null)
+        {
+            String[] actorList = line.split("[/]+");
+        }
+    }
+
 }
