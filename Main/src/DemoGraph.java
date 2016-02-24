07f6c8e72b79a8dc94190ea5b8d50b5cc579591c
hieshailey <wilsonha@bearsmail.org>
2016-02-24 08:31:57 -0500

    2/20/16

--- Main/src/DemoGraph.java
+++ Main/src/DemoGraph.java
@@ -3,82 +3,106 @@
  */
 
 import java.io.BufferedReader;
+import java.util.ArrayList;
 
 /**
  *
  * @author Michael Levet
  * @date June 09, 2015
  */
-public class DemoGraph
-    {
-        public static void main(String[] args)
-        {
-
-            //Get the buffered reader for the text file
-            BufferedReader br = FileManager.getInstance().getFile("/IMDB MPAA 21861 Movies sample.txt");
-
-            Graph graph = new Graph();
-
-            //initialize some vertices and add them to the graph
-            Vertex[] vertices = new Vertex[5];              // used to quickly make 5 nodes
-            for(int i = 0; i < vertices.length; i++){
-                vertices[i] = new Vertex("" + i);           // make a label from the counter
-                graph.addVertex(vertices[i], true);
-            }
-
-            //illustrate the fact that duplicate edges aren't added
-            for(int i = 0; i < vertices.length - 1; i++){
-                for(int j = i + 1; j < vertices.length; j++){
-                    graph.addEdge(vertices[i], vertices[j]);
-                    graph.addEdge(vertices[i], vertices[j]);
-                    graph.addEdge(vertices[j], vertices[i]);
-                }
-            }
-
-            //display the initial setup- all vertices adjacent to each other
-            for(int i = 0; i < vertices.length; i++){
-                System.out.println(vertices[i]);
-
-                for(int j = 0; j < vertices[i].getNeighborCount(); j++){
-                    System.out.println(vertices[i].getNeighbor(j));
-                }
-
-                System.out.println();
-            }
-
-            //overwrite Vertex 3
-            graph.addVertex(new Vertex("3"), true);
-            for(int i = 0; i < vertices.length; i++){
-                System.out.println(vertices[i]);
-
-                for(int j = 0; j < vertices[i].getNeighborCount(); j++){
-                    System.out.println(vertices[i].getNeighbor(j));
-                }
-
-                System.out.println();
-            }
+//public class DemoGraph
+    //{
+//        public static void main(String[] args)
+//        {
+//
+//            //Get the buffered reader for the text file
+//            BufferedReader br = FileManager.getInstance().getFile("/IMDB MPAA 21861 Movies sample.txt");
+//
+//            Graph graph = new Graph();
+//
+//            //initialize some vertices and add them to the graph
+//            ArrayList  <Vertex> vertices = new ArrayList <Vertex>();              // feed inputs into an ArrayList
+//            String line = br.readLine();
+//            int i=0;
+//            while(line!=null)
+//            {
+//                 String[] arrayName= line.split("[/]+");
+//                vertices.add( new Vertex("" + i));           // make a label from the counter
+//                graph.addVertex(vertices[i], true);
+//                i++;
+//
+//            }
+//
+//
+//
+//
 
 
-            System.out.println("Vertex 5: " + graph.getVertex("5")); //null
-            System.out.println("Vertex 3: " + graph.getVertex("3")); //Vertex 3
 
-            //true
-            System.out.println("Graph Contains {1, 2}: " +
-                    graph.containsEdge(new Edge(graph.getVertex("1"), graph.getVertex("2"))));
 
-            //true
-            System.out.println(graph.removeEdge(new Edge(graph.getVertex("1"), graph.getVertex("2"))));
 
-            //false
-            System.out.println("Graph Contains {1, 2}: " + graph.containsEdge(new Edge(graph.getVertex("1"), graph.getVertex("2"))));
 
-            //false
-            System.out.println("Graph Contains {2, 3} " + graph.containsEdge(new Edge(graph.getVertex("2"), graph.getVertex("3"))));
 
-            System.out.println(graph.containsVertex(new Vertex("1"))); //true
-            System.out.println(graph.containsVertex(new Vertex("6"))); //false
-            System.out.println(graph.removeVertex("2")); //Vertex 2
-            System.out.println(graph.vertexKeys()); //[3, 1, 0, 4]
 
-        }
-    }
+
+
+
+
+
+//
+//            //illustrate the fact that duplicate edges aren't added
+//            for(int i = 0; i < vertices.length() - 1; i++){
+//                for(int j = i + 1; j < vertices.length; j++){
+//                    graph.addEdge(vertices[i], vertices[j]);
+//                    graph.addEdge(vertices[i], vertices[j]);
+//                    graph.addEdge(vertices[j], vertices[i]);
+//                }
+//            }
+//
+//            //display the initial setup- all vertices adjacent to each other
+//            for(int i = 0; i < vertices.length; i++){
+//                System.out.println(vertices[i]);
+//
+//                for(int j = 0; j < vertices[i].getNeighborCount(); j++){
+//                    System.out.println(vertices[i].getNeighbor(j));
+//                }
+//
+//                System.out.println();
+//            }
+//
+//            //overwrite Vertex 3
+//            graph.addVertex(new Vertex("3"), true);
+//            for(int i = 0; i < vertices.length; i++){
+//                System.out.println(vertices[i]);
+//
+//                for(int j = 0; j < vertices[i].getNeighborCount(); j++){
+//                    System.out.println(vertices[i].getNeighbor(j));
+//                }
+//
+//                System.out.println();
+//            }
+//
+//
+//            System.out.println("Vertex 5: " + graph.getVertex("5")); //null
+//            System.out.println("Vertex 3: " + graph.getVertex("3")); //Vertex 3
+//
+//            //true
+//            System.out.println("Graph Contains {1, 2}: " +
+//                    graph.containsEdge(new Edge(graph.getVertex("1"), graph.getVertex("2"))));
+//
+//            //true
+//            System.out.println(graph.removeEdge(new Edge(graph.getVertex("1"), graph.getVertex("2"))));
+//
+//            //false
+//            System.out.println("Graph Contains {1, 2}: " + graph.containsEdge(new Edge(graph.getVertex("1"), graph.getVertex("2"))));
+//
+//            //false
+//            System.out.println("Graph Contains {2, 3} " + graph.containsEdge(new Edge(graph.getVertex("2"), graph.getVertex("3"))));
+//
+//            System.out.println(graph.containsVertex(new Vertex("1"))); //true
+//            System.out.println(graph.containsVertex(new Vertex("6"))); //false
+//            System.out.println(graph.removeVertex("2")); //Vertex 2
+//            System.out.println(graph.vertexKeys()); //[3, 1, 0, 4]
+//
+//        }
+//    }
--- Main/src/FileManager.java
+++ Main/src/FileManager.java
@@ -1,10 +1,10 @@
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.*;
 
-/**
- * Created by mccloskeybr on 2/16/16.
- */
+
 public class FileManager {
 
     private static FileManager fileManager;
@@ -19,7 +19,7 @@ public class FileManager {
     public BufferedReader getFile(String path){
         try {
 
-            File file = new File(path);
+            File file = new File(getClass().getResource(path).getPath());
             FileReader fr = new FileReader(file);
             BufferedReader br = new BufferedReader(fr);
 
@@ -31,4 +31,38 @@ public class FileManager {
 
         return null;
     }
-}
+
+
+    public void readLines(String path){
+        File file = new File(getClass().getResource(path).getPath());
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        ArrayList<String> actorArrayList = new ArrayList<String>();
+        HashMap<String,ArrayList<String>> actorMap = new HashMap<String,String>();
+
+        String line = br.readLine();
+        while(line!=null)
+        {
+            String[] actorArray= line.split("[/]+");
+            actorArrayList.clear();
+            for(int i=1; i<actorArray.length; i++){
+                actorArrayList.add(actorArray[i]);
+            }
+            for(int j=0; j<actorArrayList.size(); j++){
+                String k = actorArrayList.get(j);
+                if(!actorMap.containsKey(k))
+                {
+                    //arraylist stuff
+                    actorMap.put(k,actorArray[0]);
+                }
+                else
+                {
+                    String location = actorMap.get(k);
+                    //arraylist stuff
+                }
+            }
+
+
+
+        }
+}}
